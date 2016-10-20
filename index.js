691111c59f5698dd8fbf313720177fa8d0bade91
Edward Chen <yoyokko@gmail.com>
2016-10-20 17:41:00 +0800

    Update index.js
    change googleapis to lug.ustc.edu.cn

--- index.js
+++ index.js
@@ -4,31 +4,31 @@ var data = module.exports = {
   jquery: {
     versions: ['2.1.4', '2.1.3', '2.1.2', '2.1.1', '2.1.0', '2.0.3', '2.0.2', '2.0.1', '2.0.0', '1.11.1', '1.11.0', '1.10.2', '1.10.1', '1.10.0', '1.9.1', '1.9.0', '1.8.3', '1.8.2', '1.8.1', '1.8.0', '1.7.2', '1.7.1', '1.7.0', '1.6.4', '1.6.3', '1.6.2', '1.6.1', '1.6.0', '1.5.2', '1.5.1', '1.5.0', '1.4.4', '1.4.3', '1.4.2', '1.4.1', '1.4.0', '1.3.2', '1.3.1', '1.3.0', '1.2.6', '1.2.3'],
     url: function (version) {
-      return '//ajax.googleapis.com/ajax/libs/jquery/' + version + '/jquery.min.js';
+      return '//ajax.lug.ustc.edu.cn/ajax/libs/jquery/' + version + '/jquery.min.js';
     }
   },
   'jquery-ui': {
     versions: ['1.11.4', '1.11.3', '1.11.2', '1.11.1', '1.11.0', '1.10.4', '1.10.3', '1.10.2', '1.10.1', '1.10.0', '1.9.2', '1.9.1', '1.9.0', '1.8.24', '1.8.23', '1.8.22', '1.8.21', '1.8.20', '1.8.19', '1.8.18', '1.8.17', '1.8.16', '1.8.15', '1.8.14', '1.8.13', '1.8.12', '1.8.11', '1.8.10', '1.8.9', '1.8.8', '1.8.7', '1.8.6', '1.8.5', '1.8.4', '1.8.2', '1.8.1', '1.8.0', '1.7.3', '1.7.2', '1.7.1', '1.7.0', '1.6.0', '1.5.3', '1.5.2'],
     url: function (version) {
-      return '//ajax.googleapis.com/ajax/libs/jqueryui/' + version + '/jquery-ui.min.js';
+      return '//ajax.lug.ustc.edu.cn/ajax/libs/jqueryui/' + version + '/jquery-ui.min.js';
     }
   },
   dojo: {
     versions: ['1.10.4', '1.10.3', '1.10.2', '1.10.1', '1.10.0', '1.9.3', '1.9.2', '1.9.1', '1.9.0', '1.8.5', '1.8.4', '1.8.3', '1.8.2', '1.8.1', '1.8.0', '1.7.5', '1.7.4', '1.7.3', '1.7.2', '1.7.1', '1.7.0', '1.6.1', '1.6.0', '1.5.2', '1.5.1', '1.5.0', '1.4.4', '1.4.3', '1.4.1', '1.4.0', '1.3.2', '1.3.1', '1.3.0', '1.2.3', '1.2.0', '1.1.1'],
     url: function (version) {
-      return '//ajax.googleapis.com/ajax/libs/dojo/' + version + '/dojo/dojo.js';
+      return '//ajax.lug.ustc.edu.cn/ajax/libs/dojo/' + version + '/dojo/dojo.js';
     }
   },
   swfobject: {
     versions: ['2.2', '2.1'],
     url: function (version) {
-      return '//ajax.googleapis.com/ajax/libs/swfobject/' + version + '/swfobject.js';
+      return '//ajax.lug.ustc.edu.cn/ajax/libs/swfobject/' + version + '/swfobject.js';
     }
   },
   'angular-material': {
     versions: ['0.10.0', '0.9.4', '0.9.0', '0.8.3', '0.8.2', '0.8.1', '0.8.0', '0.7.1', '0.7.0', '0.6.1'],
     url: function (version) {
-      return '//ajax.googleapis.com/ajax/libs/angular_material/' + version + '/angular-material.min.js';
+      return '//ajax.lug.ustc.edu.cn/ajax/libs/angular_material/' + version + '/angular-material.min.js';
     }
   }
 };
@@ -54,7 +54,7 @@ angularFiles.forEach(function (item) {
   data[item] = {
     versions: [].concat(stableVersions).concat(unstableVersions),
     url: function (version) {
-      return '//ajax.googleapis.com/ajax/libs/angularjs/' + version + '/' + item + '.min.js';
+      return '//ajax.lug.ustc.edu.cn/ajax/libs/angularjs/' + version + '/' + item + '.min.js';
     }
   };
 });
